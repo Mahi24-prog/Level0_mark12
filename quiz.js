1fd84279b9d973e65ac6d720cec62b4b64bdfca1
Mahendra Balu Pawar <mahendrapawar444666@gmail.com>
2021-09-13 12:35:46 +0530

    Quiz part completed

---
+++ quiz.js
@@ -0,0 +1,18 @@
+var quizForm = document.querySelector(".quiz-form");
+var submitBtn = document.querySelector(".submit-btn");
+var result = document.querySelector(".result")
+
+const answerList = ["90°","right angled", "one right angle", "12, 16, 20", "Equilateral triangle"];
+
+submitBtn.addEventListener("click",()=>{
+    var score = 0;
+    var index = 0;
+    var userAns = new FormData(quizForm);
+    for(var i of userAns.values()){
+        if(i===answerList[index]){
+            score += 1;
+        }
+        index += 1;  
+    }
+    result.innerText = "The score is "+score;
+})
